a71265fe40f2595716766ab8a6960ff1271b4117
Dor Azriel <doraz4.ac.sce.ac.il>
2020-01-12 12:37:21 +0200

    כניסת משתמש + מאגר מידע משתמשים

---
+++ access reader/Data/accessreaderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace accessreader.Models
+{
+    public class accessreaderContext : DbContext
+    {
+        public accessreaderContext (DbContextOptions<accessreaderContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<accessreader.Models.UserClass> UserClass { get; set; }
+    }
+}
--- access reader/Startup.cs
+++ access reader/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using accessreader.Models;
 
 namespace access_reader
 {
@@ -33,6 +35,9 @@ namespace access_reader
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<accessreaderContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("accessreaderContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
